e984cd9792625e1f584161dca0188c00d6e82993
manastyagi <manasvi.tyagi@gmail.com>
2020-07-08 13:19:07 +1200

    Updates

---
+++ precache-manifest.cf376b809bf5653df41e73ccd9718003.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9046b9dfd28ce8f7dcf72fe871de87a0",
+    "url": "/react_iot/index.html"
+  },
+  {
+    "revision": "791499da90dd6e71601a",
+    "url": "/react_iot/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "b4032d86497ba68d12de",
+    "url": "/react_iot/static/css/main.444a1210.chunk.css"
+  },
+  {
+    "revision": "791499da90dd6e71601a",
+    "url": "/react_iot/static/js/2.aa721ce1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react_iot/static/js/2.aa721ce1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4032d86497ba68d12de",
+    "url": "/react_iot/static/js/main.19c8a629.chunk.js"
+  },
+  {
+    "revision": "f40585df9283454d1d2e",
+    "url": "/react_iot/static/js/runtime-main.9e009422.js"
+  }
+]);
